8f1b57a723d0d92136b40e16879cdf76440791c2
BlueEyedHush <blueeyedhush@gmail.com>
2016-06-08 13:35:08 +0200

    fabric-based automation

---
+++ fabfile.py
@@ -0,0 +1,35 @@
+from fabric.api import *
+
+env.hosts = ['shnek.tk']
+location = '~/smartgrid'
+repo_url = 'https://github.com/kroczi/TIR_energy_flow.git'
+cmd = 'python {0}/main.py -n {1} -d 0 -I'
+
+def prerequisties():
+    run('yes | sudo apt-get install python-pip')
+    run('sudo pip install python-setuptools')
+
+def install():
+    global location, repo_url
+    run('git clone {0} {1}'.format(repo_url, location))
+    run('pip install -e {0} --user'.format(location))
+
+def execute(node_id):
+    global location, cmd
+    run('nohup {0} >& /tmp/smartgrid.log < /dev/null &'.format(cmd.format(location, node_id)), pty=False)
+
+def setup_all():
+    prerequisties()
+    install()
+    execute_all()
+
+
+def execute_all():
+    oldhosts = env.hosts
+    for node_id in range(0, len(oldhosts)):
+        env.hosts = [oldhosts[node_id]]
+        execute(node_id)
+    env.hosts = oldhosts
+
+def reboot_all():
+    run('reboot -f')
